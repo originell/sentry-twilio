5f4ecc7433c39cbe2a24a63e2527f8f9f38d67b4
Luis Nell <luis.nell@simpleloop.com>
2017-02-01 17:14:34 +0100

    puts auth token into secret field.

--- src/sentry_twilio/plugin.py
+++ src/sentry_twilio/plugin.py
@@ -17,12 +17,30 @@ class TwilioPlugin(notify.NotificationPlugin):
     slug = 'twilio'
     description = 'Send notifications via Twilio Programmable SMS.'
 
+    def get_secret_field_config(secret, help_text, include_prefix=False, **kwargs):
+        # shamelessly taken from the official sentry-plugins repo
+        has_saved_value = bool(secret)
+        saved_text = 'Only enter a new value if you wish to update the existing one. '
+        context = {
+            'type': 'secret',
+            'has_saved_value': has_saved_value,
+            'prefix': (secret or '')[:4] if include_prefix else '',
+            'required': not has_saved_value,
+            'help': '%s%s' % ((saved_text if has_saved_value else ''), help_text)
+        }
+        context.update(kwargs)
+        return context
+
     def is_configured(self, project):
         return (bool(self.get_option('account_sid', project))
                 and bool(self.get_option('auth_token', project))
                 and bool(self.get_option('phone_number', project)))
 
     def get_config(self, project, **kwargs):
+        auth_token_key = self.get_option('auth_token', project)
+        auth_token = get_secret_field_config('auth_token',
+                                             'Your Twilio Auth Token.',
+                                             include_prefix=True)
         return [
             {
                 'name': 'account_sid',
@@ -31,18 +49,21 @@ class TwilioPlugin(notify.NotificationPlugin):
                 'placeholder': 'e.g. AA0000eeee0000000cc00000b00eeccc00',
                 'required': True,
                 'help': 'Your Twilio Account SID.',
-            }, {
-                'name': 'auth_token',
-                'label': 'Auth Token',
+            },
+            auth_token,
+            {
+                'name': 'phone_number',
+                'label': 'SMS Number',
                 'type': 'string',
-                'placeholder': 'e.g. AA1111eeee1111111cc11111b11eeccc11',
+                'placeholder': 'e.g. +10110010001',
                 'required': True,
-                'help': 'Your Twilio Auth Token.',
-            }, {
+                'help': 'Your Twilio Phone Number with Programmable SMS support.',
+            },
+            {
                 'name': 'recipients',
                 'label': 'Recipients',
                 'type': 'string',
-                'placeholder': 'e.g. +10110010001, +436660000,...',
+                'placeholder': 'e.g. +10110011010, +436660000,...',
                 'required': True,
                 'help': ('A comma separated list of notification recipient\'s'
                          'phone numbers. Note that if you have a Twilio Trial'
